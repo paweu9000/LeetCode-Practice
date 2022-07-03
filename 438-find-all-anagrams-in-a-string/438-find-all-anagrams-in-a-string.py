320dea053b9d70e2a948ac3a149e4a2d97c9c625
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2022-07-03 18:22:54 +0200

    Time: 414 ms (25.57%), Space: 15.3 MB (33.40%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        res = []
+        pCounter = Counter(p)
+        sCounter = Counter(s[:len(p)-1])
+        for i in range(len(p)-1, len(s)):
+            sCounter[s[i]] += 1
+            if sCounter == pCounter:
+                res.append(i-len(p)+1)
+            sCounter[s[i-len(p)+1]] -= 1
+            if sCounter[s[i-len(p)+1]] == 0:
+                del sCounter[s[i-len(p)+1]]
+        return res
