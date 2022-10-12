73487e5721b3eced21edb1d05bf6b9fdec2ec895
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2022-10-12 23:32:51 +0200

    Time: 2 ms (81.56%), Space: 48.5 MB (30.25%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.java
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> result = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
         for(int i = 1; i <= n; i++) {
             if(i % 3 == 0 && i % 5 == 0) {
                 result.add("FizzBuzz");
