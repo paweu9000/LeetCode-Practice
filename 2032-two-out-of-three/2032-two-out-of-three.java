439987d910925057309a9345985e0ec0ef8f02b2
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2022-11-17 10:24:10 +0100

    Time: 14 ms (31.86%), Space: 48.2 MB (18.21%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Solution {
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        Set<Integer> set1 = new HashSet<>(), set2 = new HashSet<>(), set3 = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
+        for(int i: nums1){set1.add(i); set.add(i);}
+        for(int i: nums2){set2.add(i); set.add(i);}
+        for(int i: nums3){set3.add(i); set.add(i);}
+        List<Integer> result = new ArrayList<>();
+        for(int i: set) {
+            if(set1.contains(i) && set2.contains(i) || set2.contains(i) && set3.contains(i) ||
+              set3.contains(i) && set1.contains(i)) result.add(i);
+        }
+        return result;
+    }
+}
