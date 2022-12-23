538965363e0ff0677da6f99a8040673f303f534f
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2022-12-23 19:26:44 +0100

    Time: 0 ms (100.00%), Space: 42 MB (46.25%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int zeros = 0;
+        int ones = 0;
+        
+        for(int student: students) {
+            if(student == 1) {
+                ones++;
+            } else {
+                zeros++;
+            }
+        }
+        
+        for(int sandwich: sandwiches) {
+            if(sandwich == 0) {
+                if(zeros == 0) {
+                    return ones;
+                }
+                zeros--;
+            } else {
+                if(ones == 0) {
+                    return zeros;
+                }
+                ones--;
+            }
+        }
+        return 0;
+    }
+}
