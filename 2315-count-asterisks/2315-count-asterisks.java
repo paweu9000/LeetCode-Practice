e1a669446e4c4653bc00f58fc3b4f761cdceb216
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2022-12-26 11:03:09 +0100

    Time: 2 ms (87.19%), Space: 40.7 MB (75.18%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int count = 0;
+        int bars = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '*' && bars % 2 == 0) {
+                count++;
+            }
+            if(s.charAt(i) == '|') bars++;
+        }
+        return count;
+    }
+}
