976cae5f1574b775d44148d5f97d1392fe52ad01
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2023-05-15 17:37:57 +0200

    Time: 1 ms (68.12%), Space: 40 MB (34.27%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int time = arrivalTime + delayedTime;
+        return time >= 24 ? time - 24 : time;
+    }
+}
