45909dacad91410cbd53d07438434b3f33234508
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2023-06-01 10:04:40 +0200

    Time: 7 ms (76.25%), Space: 47.3 MB (13.26%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s: strs) {
+            char[] cArr = s.toCharArray();
+            Arrays.sort(cArr);
+            String key = String.valueOf(cArr);
+            if (!map.containsKey(key)) map.put(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
