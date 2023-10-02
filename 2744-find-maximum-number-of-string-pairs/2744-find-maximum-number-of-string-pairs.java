0753eee59818d7f027bc4c454c5b5a8c4e5f278e
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2023-10-02 18:29:03 +0200

    Time: 6 ms (32.41%), Space: 43.7 MB (18.54%) - LeetHub

--- 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.java
+++ 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.java
@@ -3,13 +3,7 @@ class Solution {
         int sum = 0;
         for (int i = 0; i < words.length-1; i++) {
             for (int j = i+1; j < words.length; j++) {
-                if (words[i].equals(words[j])) {
-                    ++ sum;
-                } else if 
-                    (new StringBuilder(words[i]).reverse().toString().equals(words[j]) ||
-                    new StringBuilder(words[j]).reverse().toString().equals(words[i])) {
-                        ++ sum;
-                    }
+                if (new StringBuilder(words[j]).reverse().toString().equals(words[i])) ++ sum;
             }
         }
         return sum;        
