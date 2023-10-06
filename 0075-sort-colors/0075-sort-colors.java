7de84c049719c42d463eb2bace290a1cb30b9d74
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2023-10-06 08:21:36 +0200

    Time: 0 ms (100.00%), Space: 40.8 MB (58.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int low = 0, high = nums.length-1, index = 0;
+        while (index <= high) {
+            if (nums[index] == 0) {
+                nums[index] = nums[low];
+                nums[low] = 0;
+                low++;
+            }
+            if (nums[index] == 2) {
+                nums[index] = nums[high];
+                nums[high] = 2;
+                high--;
+                index--;
+            }
+            index++;
+        }
     }
 }
