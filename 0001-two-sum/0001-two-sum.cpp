91513745b93d3fface49be69844fd84729e644ca
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2023-10-20 16:38:40 +0200

    Time: 279 ms (32.25%), Space: 10.5 MB (74.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         for (int i = 0; i < nums.size()-1; i++) {
             for (int j = i+1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) return vector<int>{i, j};
+                if (nums[i] + nums[j] == target) return {i, j};
             }
         }
-        return vector<int>{};
+        return {};
     }
 };
