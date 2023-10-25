813a5ee537cb4b8b1992ea04b2571102b62ae467
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2023-10-25 09:00:47 +0200

    Time: 0 ms (100.00%), Space: 6.9 MB (36.63%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s[i] != ' ') len++;
+            else if (len == 0 and s[i] == ' ') continue;
+            else return len;
+        }
+        return len;
+    }
+};
